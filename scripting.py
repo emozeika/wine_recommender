1033f6cd0b7c0538c50b78a9adc014bc32fce818
emozeika <mozeikaeric@gmail.com>
2022-09-18 18:43:14 -0400

    changing project to wine recommender

--- scripting.py
+++ scripting.py
@@ -1,8 +1,20 @@
 
+import requests
+
+
+class Vivino(object):
+    BASEURL_ = 'https://www.vivino.com/api/'
+    REGIONS_ = '/regions'
+
+    def __init__(self):
+        pass
+
+    def get_regions(self):
+        url = self.BASEURL_  + self.REGIONS_
+        requests.get(url)
 
 
 
-print('Hello World')
 
 if __name__  == '__main__':
-    pass
+    print(Vivino().BASEURL_)
