8cc1f1f3a4bb2073c296acda905cf145e50ef719
emozeika <mozeikaeric@gmail.com>
2022-09-18 22:57:47 -0400

    added pandas as a package and wrote testing code for importing data into postgres

--- scripting.py
+++ scripting.py
@@ -1,20 +1,25 @@
-
-import requests
-
-
-class Vivino(object):
-    BASEURL_ = 'https://www.vivino.com/api/'
-    REGIONS_ = '/regions'
-
-    def __init__(self):
-        pass
-
-    def get_regions(self):
-        url = self.BASEURL_  + self.REGIONS_
-        requests.get(url)
+from sqlalchemy import create_engine
+import psycopg2
+import io
+import pandas as pd
 
 
+df = pd.read_csv('x60.csv')
+print(df.head())
+engine = create_engine('postgresql+psycopg2://postgres:Emoz3832!@localhost:5432/VIVINO')
+
+df.head(0).to_sql('table_name', engine, if_exists='replace',index=False) #drops old table and creates new empty table
+
+conn = engine.raw_connection()
+cur = conn.cursor()
+output = io.StringIO()
+df.to_csv(output, sep='\t', header=False, index=False)
+output.seek(0)
+contents = output.getvalue()
+cur.copy_from(output, 'table_name', null="") # null values become ''
+conn.commit()
 
 
-if __name__  == '__main__':
-    print(Vivino().BASEURL_)
+
+
+
