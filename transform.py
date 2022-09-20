955d621ddc3ee6a926279ce9f4aa348c73dfd88d
emozeika <mozeikaeric@gmail.com>
2022-09-19 21:42:03 -0400

    added functions in transformation layer and fixed vintage function in vivino

--- transform.py
+++ transform.py
@@ -41,16 +41,87 @@ class Transform(object):
         regions_data = []
         for region in raw_data['regions']:
             region_data = []
-            #appending name and english name
-            region_data.extend((region['name'], region['name_en']))
-    
+
+            #appending id, name and english name
+            region_data.extend((region['id'], region['name'], region['name_en']))
+            #appending country info
+            region_data.extend((region['country']['name'], region['country']['code']))
 
             #append low to data frame
             regions_data.append(region_data)
 
-
-        cols = ['name', 'name_english']
+        cols = ['id', 'name', 'name_english', 'country_name', 'country_code']
         df = pd.DataFrame(regions_data, columns=cols)
+
+        return df
+
+
+
+    def create_grape_table(self):
+        '''
+        Function to create the regions table from raw data
+        '''
+
+        raw_data = self.load_file('grape.json')
+
+        grapes_data = []
+        for grape in raw_data['grapes']:
+            grape_data = []
+
+            #append grape id, name, seo name
+            grape_data.extend((
+                grape['id'],
+                grape['name'],
+                grape['seo_name']
+            ))
+
+            grapes_data.append(grape_data)
+
+        cols = ['id', 'name', 'seo_name']
+        df = pd.DataFrame(grapes_data, columns=cols)
+
+        return df
+
+
+
+    def create_vintage_table(self):
+        '''
+        Function to create the vintage table from raw data
+        '''
+
+        raw_data = self.load_file('vintage.json')
+
+        vintages_data = []
+        for vintage in raw_data['vintages']:
+            vintage_data = []
+            #getting id, name and seo_name
+            vintage_data.extend((
+                            vintage['vintage'].get('id', None),                                   #vintage id
+                            vintage['vintage']['name'].replace('\t', 't'),                        #vintage name
+                            vintage['vintage'].get('seo_name', None),                             #vintage search engine name
+                            vintage['vintage'].get('year', None),                                 #vintage year
+                            vintage['vintage']['statistics'].get('ratings_average', None),        #avergae rating
+                            vintage['vintage']['statistics'].get('ratings_count', None),          #number of ratings
+                            vintage['vintage']['statistics'].get('reviews_count', None),          #number of reviews
+                            vintage['vintage']['wine'].get('id', None)                            #wine id
+                            ))
+
+            vintages_data.append(vintage_data)
+
+        cols = ['id', 'name', 'seo_name', 'year', 'avg_rating', 'rating_count', 'review_count', 'wine_id']
+        df = pd.DataFrame(vintages_data, columns=cols)
+
+        return df
+
+
+
+    def create_table(self, table_name):
+        if table_name == 'region':
+            df = self.create_region_table()
+        elif table_name == 'grape':
+            df = self.create_grape_table()
+        elif table_name == 'vintage':
+            df = self.create_vintage_table()
 
         return df
 
@@ -62,8 +133,20 @@ class Transform(object):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    d = Transform().create_region_table()
+    d = Transform().create_table('region')
     PostGresClient().insert_data_from_df(d, 'region')
     
    
--- vivino.py
+++ vivino.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from urllib import response
 import requests
 import json
-
+import math
 
 #loading in necessary variables from config file
 from config import RAW_DATA_PATH
@@ -15,16 +15,18 @@ class Vivino(object):
     This is a class to extract data from Vivino.com wine library
     '''
 
-    BASEURL_ = 'https://www.vivino.com/api/'
+    BASEURL_ = 'https://www.vivino.com/api'
     ENDPOINT_ = {
         'region' : '/regions',
         'style' : '/wine_styles',
         'country' : '/countries',
         'grape' : '/grapes',
-        'vintage' : '/explore/explore?price_range_min=1&page={}&per_page=50'   
+        'vintage': '/explore/explore?region_ids[]={}&page={}&per_page=50'
+        #'vintage' : '/explore/explore?price_range_min=1&page={}&per_page=50'   
     }
     HEADERS_ =  { 
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
+        #"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0"
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
     }
 
     def __init__(self):
@@ -32,7 +34,7 @@ class Vivino(object):
 
 
 
-    def get_vintage_data(self):
+    def get_vintage_data_old(self):
         #need to add vintages to the original data in a loop since it will not 
         #allow a straight pull
 
@@ -42,9 +44,10 @@ class Vivino(object):
         response.pop('records', None)
 
         #finding the numbeer of loops that need to be made
-        num_pages = response['explore_vintage']['records_matched'] 
+        num_pages = response['explore_vintage']['records_matched'] //50 + 1
         print(num_pages)
-        for i in range(2,5): #change this after testing
+        for i in range(2,num_pages): 
+            print(i)
             #making new request
             url = self.BASEURL_ + self.ENDPOINT_['vintage'].format(i)
             new_matches = requests.get(url, headers=self.HEADERS_).json()['explore_vintage']['matches']
@@ -55,10 +58,45 @@ class Vivino(object):
 
         return response
 
+    
+    def get_vintage_data(self):
+
+        #get regions and ids because we have to loop through there
+        url = self.BASEURL_ + self.ENDPOINT_['region'] 
+        response = requests.get(url, headers=self.HEADERS_).json()
+
+        #making a list of region ids to loop through
+        region_ids = []
+        for region in response['regions']:
+            id = region['id']
+            region_ids.append(region['id'])
+
+        #initializing data 
+        data = {'vintages' : []}
+
+        for num, id in enumerate(region_ids):
+            if num % 100 == 0: print(f'Region {num} of {len(region_ids)}')
+
+            #need to run a check first to get the number of matches
+            url = self.BASEURL_ + self.ENDPOINT_['vintage'].format(id, 1)
+            response = requests.get(url, headers=self.HEADERS_).json()
+            num_matches = response['explore_vintage']['records_matched']
+
+            #if statement if we can pull all the records in 1 request else we loop through pages
+            if num_matches <= 50:
+                data['vintages'] += response['explore_vintage']['matches']  
+            else:
+                for i in range(1, math.ceil(num_matches/50)+1):
+                    url = self.BASEURL_ + self.ENDPOINT_['vintage'].format(id, i)
+                    response = requests.get(url, headers=self.HEADERS_).json()
+                    data['vintages'] += response['explore_vintage']['matches']
+            
+        
+        return data
+
 
     
     def get_data(self, data_type):
-
         
         if data_type == 'vintage':
             return self.get_vintage_data()
@@ -83,6 +121,6 @@ class Vivino(object):
 
 if __name__ == '__main__':
     
-    r = Vivino().get_data('vintage')
-    print(len(r['explore_vintage']['matches']))
+    Vivino().save_data('vintage')
+    #print(len(r['explore_vintage']['matches']))
 
